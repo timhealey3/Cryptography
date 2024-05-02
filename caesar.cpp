e0ea573426a173fcd04bd54e90f7de9a387fc8d9
timhealey3 <timhealey34@gmail.com>
2024-05-02 15:42:31 -0400

    caesar working

--- caesar.cpp
+++ caesar.cpp
@@ -7,19 +7,30 @@ std::string encryptCaesar(std::string word) {
   int shiftVal;
   int leftOver;
   const int lower_a = 97;
+  const int upper_a = 65;
   //const int lower_z = 122;
+  std::string answer = "";
 
   for (char c : word) {
     asciiVal = static_cast<int>(c);
     shiftVal = asciiVal + 3;
-    if (shiftVal > 96){
-    if (shiftVal > 122) {
-      leftOver = shiftVal - 122;
-      shiftVal = lower_a + leftOver - 1;
+    // if letter is upper case
+    if (asciiVal > 64 && asciiVal < 91) {
+      if (shiftVal > 90) {
+        leftOver = shiftVal - 90;
+        shiftVal = upper_a + leftOver - 1;
+      }
     }
-    std::cout << shiftVal << std::endl;
-    // if above some num move it back to 90?
+    // if letter is lower case
+    if (asciiVal > 96 && asciiVal < 123){
+      if (shiftVal > 122) {
+        leftOver = shiftVal - 122;
+        shiftVal = lower_a + leftOver - 1;
+      }
+    }
+    char charVal = static_cast<char>(shiftVal);
+    answer.push_back(charVal);
   }
-  }
-  return word;
+
+  return answer;
 }
